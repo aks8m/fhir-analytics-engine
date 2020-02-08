de23dd4cacd692d701a3b2a94b52b070140eb7e9
Andrew Sills <andrew.k.sills@gmail.com>
2020-02-07 19:17:23 -0500

    Working initial commit based on Spring Inializer tool and some optomizations after looking into maven dependencies

---
+++ src/main/java/com/github/aks8m/fae/FhirAnalyticsEngineApplication.java
@@ -0,0 +1,13 @@
+package com.github.aks8m.fae;
+
+import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirAnalyticsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirAnalyticsEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aks8m/fae/FhirAnalyticsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aks8m.fae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirAnalyticsEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
